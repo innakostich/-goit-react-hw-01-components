a6627a1cadd1640aa9060a6562579b9445a2d145
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-01 10:41:55 +0200

    adds components

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,2 +1,10 @@
-// import PropTypes from 'prop-types';
-// import css from './Container.module.css';
+import PropTypes from 'prop-types';
+import css from './Container.module.css';
+
+export const Container = ({ children }) => {
+  return <div className={css.container}>{children}</div>;
+};
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -1,12 +1,13 @@
 
-import data from './data.json';
-import Statistics from './Statistics';
+// import data from './data.json';
+// import Statistics from './Statistics';
 
-const App = () => (
-  <div>
-    <Statistics title="Upload stats" stats={data} />
-    <Statistics stats={data} />
-  </div>
-);
+// const App = () => (
+//   <div>
+//     <Statistics title="Upload stats" stats={data} />
+//     <Statistics stats={data} />
+//   </div>
+// );
 
-export default App;
+// export default App;
+// export { Statistics } from './Statistics';
