77ba0b50cdbffe154ee8d8900a18e26803427af0
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-02 19:25:36 +0200

    adds css styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,20 @@
 
 import PropTypes from 'prop-types';
-import s from './Statistics.module.css';
+import css from './Statistics.module.css';
 
-const colors = ['#984ED0', '#CDD25A', '#FDB05E', '#0475ED', '#F02C6E'];
+const colors = ['#EB7474', '#4CB8C4', '#F9D56E', '#A7C080', '#E39D7F'];
+
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className={s.statistics}>
-      {title && <h2 className={s.title}>{title}</h2>}
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className={s.list}>
+      <ul className={css.list}>
         {stats.map(({ id, label, percentage }, index) => (
-          <li className={s.item} key={id} style={{ background: colors[index] }}>
-            <span className={s.label}>{label}</span>
-            <span className={s.percentage}>{percentage}</span>
+          <li className={css.item} key={id} style={{ background: colors[index] }}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}</span>
           </li>
         ))}
       </ul>
